cb20d292e02ce928121c89b5bed222778aff9728
aremoe <danis_zs71@student.itstep.org>
2025-07-04 12:02:38 +0300

    + пагінація

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Car
 from .forms import CarForm
 
 def all_cars(request):
-    cars = Car.objects.all()
-    return render(request, 'cars/all_cars.html', {'cars': cars})
-
+    car_list = Car.objects.all()
+    paginator = Paginator(car_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'cars/all_cars.html', {'page_obj': page_obj})
+    
 def available_cars(request):
     cars = Car.objects.filter(available=True)
     return render(request, 'cars/available_cars.html', {'cars': cars})
